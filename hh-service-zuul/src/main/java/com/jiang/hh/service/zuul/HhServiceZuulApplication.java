3e0b7bc7a12687f96627810694d53c2ab1e5a1a4
xiangjiangcheng <jiangcheng.xiang@qq.com>
2020-03-23 14:23:41 +0800

    zuul做路由

---
+++ hh-service-zuul/src/main/java/com/jiang/hh/service/zuul/HhServiceZuulApplication.java
@@ -0,0 +1,19 @@
+package com.jiang.hh.service.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class HhServiceZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhServiceZuulApplication.class, args);
+	}
+
+}
