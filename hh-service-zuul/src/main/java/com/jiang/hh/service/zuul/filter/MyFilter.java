377d6a5e40297c95f253d517f7835e5e5e9d4db5
xiangjiangcheng <jiangcheng.xiang@qq.com>
2020-03-23 14:36:53 +0800

    添加filer，进行过滤，安全验证

---
+++ hh-service-zuul/src/main/java/com/jiang/hh/service/zuul/filter/MyFilter.java
@@ -0,0 +1,64 @@
+package com.jiang.hh.service.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 过滤，安全验证
+ */
+@Component
+@Slf4j
+public class MyFilter extends ZuulFilter {
+
+    /**
+     * 返回一个字符串代表过滤器的类型，在zuul中定义了四种不同生命周期的过滤器类型
+     * pre：路由之前
+     * routing：路由之时
+     * post： 路由之后
+     * error：发送错误调用
+     */
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    /**
+     * 这里可以写逻辑判断，是否要过滤
+     * @return false
+     */
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("token");
+        if (accessToken == null) {
+            log.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            } catch (Exception e) {
+            }
+
+            return null;
+        }
+        log.info("ok");
+        return null;
+    }
+}
